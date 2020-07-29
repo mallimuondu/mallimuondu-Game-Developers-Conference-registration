819cbd14d7afe922b30fd4fadac8cd008ffc12b4
mallimuondu <mallimuondu@gmail.com>
2020-07-29 08:56:24 +0300

    aded code

--- work_one.py
+++ work_one.py
@@ -1,58 +1,68 @@
-complete = False
-user = {"malli" : "Malli2010", "nesh" : "1234" }
- 
-while not complete:
-    username = input("Username: ")
-    password = input("Password: ")
-    if username == user and password == password:
-        continue
-    elif username not in user:
-        print("This is not a valid username, input username again!")
-        continue
-    elif password != user[username]:
-        
-        print("Password is not valid for username.")
-        continue
-    elif password == user[username]:
-        print("Welcome "+username)
-        print("Thank you for logging on. ")
-        complete = True
-        print ("Username and Password Validated in Python")     
-    elif password == user[username]:
-        print("Welcome "+username)
-        print("Thank you for logging in. ")
-        complete = True  
+from tinydb import TinyDB,Query
+import datetime as time
+def datetieme():
+    now = time.datetime.now()
+    hour = now.hour
+    if hour < 12:
+        greating =  "Good morning"
+    elif hour > 12 and hour < 17:
+        greating =  "Good afternoon"
+
+    elif hour > 17 and hour < 19:
+        greating =  "Good evening"
+    else:
+        greating = 'Good night'
+    print(greating)
+datetieme()
+
+
+def login():
+    complete = False
+    user = {"malli" : "Malli2010", "Nesh" : "1" }
+
+    while not complete:
+        username = input("Username: ")
+        password = input("Password: ")
+        if username == user and password == password:
+            continue
+        elif username not in user:
+            print("This is not a valid username, input username again!")
+            continue
+        elif password != user[username]:
+
+            print("Password is not valid for username.")
+            continue
+        elif password == user[username]:
+            print("Welcome username ")
+            print("Thank you for logging on. ")
+            complete = True
+            print ("Username and Password Validated in Python")     
+login()
 def adding_name():
-    with open('person.txt','a') as f:
-        asa = input("Participant first name :  ")
-        if asa == " ":
-            print("enter a name")
-            adding_name()
-        else:
-            second = input("Participant Second Name: ")
-            if second == " ":
-                print("enter a name")
-                adding_name()
-            else:
-                f.write(asa)
-                f.write(second)
-                f.write("\n")
-        
+    asa = input("enter first name")
+    second = input("Participant Second Name: ") 
+    name3 = asa + second
+    if name3 == "":
+        print("enter a name")
+    
+    else:
+        dicti = {"name1":asa, "name2":second}
+        db = TinyDB('people.json')
+        db.insert(dicti)
 adding_name()
-
-
 def pay():
     print("we have the following tickets. ")
-    with open('tickets.txt','a') as f:   
-        s = ('''
-        1.ordinary
-        2.V.I.P
-        3.Golden
-
-        ''')
-        print(s)
+    s = ('''
+    1.ordinary
+    2.V.I.P
+    3.Golden
+    ''')
+    print(s)
+    with open('tickets.txt','a') as f: 
         b = input("Choose one of the tickets : ")
-        if b == " ":
+        f.write(b)
+        f.write("\n")
+        if b == "":
             print("enter one of the tickets")
             pay()
         else:
